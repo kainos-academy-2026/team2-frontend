5eb46dd4c2d7507192eb588c6dae75b6bcdfa3e9
HollyMckibbin <holly.mckibbin@kainos.com>
2026-07-06 14:55:46 +0100

    Finished front end

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ const PORT = Number(process.env.PORT) || 3001;
 app.listen(PORT, () => {
 	console.log('Server running on http://localhost:${PORT}');
 });
+=======
+  console.log("Server running on http://localhost:" + PORT);
+});
+>>>>>>> c90417e (Finished front end)
