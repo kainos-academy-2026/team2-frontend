ae2cc61b020843d70546587c8de39dc0895c4d3f
HollyMckibbin <holly.mckibbin@kainos.com>
2026-07-06 14:55:46 +0100

    Finished front end

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+import app from "./app";
 
 const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
+<<<<<<< HEAD
 	console.log("Server running on http://localhost:" + PORT);
 });
+=======
+  console.log("Server running on http://localhost:" + PORT);
+});
+>>>>>>> c90417e (Finished front end)
