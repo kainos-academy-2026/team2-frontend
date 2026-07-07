152b9308ca72fb6ec4de4c5236139f5b824d3ab4
HollyMckibbin <holly.mckibbin@kainos.com>
2026-07-07 14:56:19 +0100

    small-changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import app from "./app";
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+	console.log('Server running on http://localhost:${PORT}');
 });
-=======
-  console.log("Server running on http://localhost:" + PORT);
-});
->>>>>>> c90417e (Finished front end)
