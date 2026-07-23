cadcc2d598934f0a787a69ac784fab73935c194e
HollyMckibbin <holly.mckibbin@kainos.com>
2026-07-23 14:00:46 +0100

    Holly tries testing

--- framework/playwright.config.ts
+++ framework/playwright.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "node:path";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 
 /**
  * Generate report name based on date/time (YYYY-MM-DD_HH-mm-ss format)
@@ -46,14 +47,12 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('')`. */
-		// baseURL: 'http://localhost:3000',
+		baseURL: "http://localhost:3000",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
-		traceDir: `${reportDir}/${reportName}/traces`,
 		/* Screenshot on failure */
 		screenshot: "only-on-failure",
-		screenshotDir: `${reportDir}/${reportName}/screenshots`,
 	},
 
 	/* Configure projects for major browsers */
@@ -86,9 +85,12 @@ export default defineConfig({
 	globalTeardown: require.resolve("./globalTeardown.ts"),
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://localhost:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: [
+		{
+			command: "cd .. && npm run dev",
+			url: "http://localhost:3000/login",
+			reuseExistingServer: !process.env.CI,
+			timeout: 120 * 1000,
+		},
+	],
 });
